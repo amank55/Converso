0cce0c77409002248eb2fa283ad05c084c9d98e5
amank55 <amankumarr2005ak@gmail.com>
2025-07-17 16:57:19 +0530

    Update dependencies, add Clerk integration, and enhance global styles. Introduce new color variables and improve page structure with a welcome message and button component. Remove unused SVG files.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+   <div>
+    <h1 className='text-2xl underline'>Welcome to my SaaS app</h1>
+    <Button>
+      Let's get started
+    </Button>
+   </div>
   )
 }
 
